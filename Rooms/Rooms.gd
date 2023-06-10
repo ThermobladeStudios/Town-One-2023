b129722968bd5c7cd3e7df62a2b1770fb2874d9d
Thermoblade Studios <thermobladestudios@gmail.com>
2023-06-10 15:08:48 -0700

    Dice Roll & Door Generation

--- Rooms/Rooms.gd
+++ Rooms/Rooms.gd
@@ -1,13 +1,18 @@
 extends Node2D
 
+var RNG = RandomNumberGenerator.new()
+var DoorsOpen = [0, 0, 0, 0]
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	var Doors = [$Door1, $Door2, $Door3, $Door4]
+	DoorsOpen[RNG.randi_range(0, 3)] = 1
+	for x in range(DoorsOpen.size()):
+		if DoorsOpen[x] != 1 and RNG.randi_range(1, 4) == 1:
+			DoorsOpen[x] = 1
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-	
-
+	for x in range(DoorsOpen.size()):
+		print(Doors)
+		if DoorsOpen[x] == 1:
+			Doors[x].get_node("CollisionShape2D").disabled = false
+		else:
+			Doors[x].get_node("CollisionShape2D").disabled = true
