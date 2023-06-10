37427eab6d08e3a8ca95ebb162c49d78c9b230a2
James Shi <jamesshi117@gmail.com>
2023-06-10 14:40:47 -0700

    Panda update script

--- Panda/Panda.gd
+++ Panda/Panda.gd
@@ -16,7 +16,7 @@ func _physics_process(delta):
 		# Player is inside the radius, so panda should look for Bamboo
 		var bamboo_target = find_closest_bamboo()
 		if bamboo_target != null:
-			target = bamboo_target
+			target = bamboo_target	
 		
 	if target != null:
 		var target_direction = (target - position).normalized()
@@ -30,9 +30,63 @@ func find_closest_bamboo():
 	var closest_distance = INF
 	var bamboo =  get_parent().get_node("Bamboo")  # Assuming you've added all your Bamboo nodes to a group called "Bamboo"
 	var distance = position.distance_to(bamboo.position)
-	print('Following bamboo')
 	if distance < closest_distance:
 		closest_distance = distance
 		closest_bamboo = bamboo.position
 			
 	return closest_bamboo  # Return null if no bamboo is found
+	
+	
+@export var health = 100
+@export var attack = 10
+@export var attack_range = 50
+@onready var attack_area = $Area2D
+var target_bamboo = null
+
+func _ready():
+	attack_area.connect("body_entered", Callable(self, "_on_Bamboo_entered"))
+	attack_area.connect("body_exited", Callable(self, "_on_Bamboo_exited"))
+
+func _on_Bamboo_entered(body):
+	print("Bamboo body entered")
+	print(body.get_name())
+	if body.get_name() == "Bamboo":
+		target_bamboo = body
+		start_combat()
+
+func _on_Bamboo_exited(body):
+	if body == target_bamboo:
+		target_bamboo = null
+		stop_combat()
+
+func start_combat():
+	print("Starting combat")
+#	show_attack()
+
+func stop_combat():
+	print("Stopping combat")
+	
+
+
+@onready var attack_label = $AttackLabel
+@onready var attack_tween = get_tree().create_tween()
+
+func show_attack():
+	attack_label.text = "-%d hp" % attack
+	attack_label.show()
+	
+	print(attack_label)
+	print(attack_label.rect_position, attack_label.rect_position + Vector2(0, -50))  # should not be the same
+	print(attack_label.modulate, Color(0, 0, 0, 0))  # should not be the same
+
+
+	# Animate label to make it float upwards and disappear
+	attack_tween.interpolate_property(attack_label, "rect_position", attack_label.rect_position, attack_label.rect_position + Vector2(0, -50), 1, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+	attack_tween.interpolate_property(attack_label, "modulate", attack_label.modulate, Color(0, 0, 0, 0), 1, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+	attack_tween.start()
+#	await(attack_tween, "tween_completed")
+	attack_label.hide()
+	attack_label.rect_position = Vector2(0, 0)  # Reset label position
+
+
+	
