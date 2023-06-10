a73863a000c1aadcdebf36947c945136c7567547
Mentholantum <harrisontseng7@gmail.com>
2023-06-10 13:00:49 -0700

    GlobalData

--- Character.gd
+++ Character.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 @export var speed = 100
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	var input_direction = Vector2(Input.get_action_strength("Right") - Input.get_action_strength("Left") , Input.get_action_strength("Down") - Input.get_action_strength("Up")).normalized()
